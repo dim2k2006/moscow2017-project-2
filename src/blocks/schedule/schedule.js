b18c556a759ca850ad85fac60077b250e91f7a5a
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-03-19 13:22:58 +0300

    Update static

--- src/blocks/schedule/schedule.js
+++ src/blocks/schedule/schedule.js
@@ -11,12 +11,43 @@
         var self = this;
 
         self.container = document.querySelector('.schedule');
+        self.endPoint = 'https://raw.githubusercontent.com/dim2k2006/moscow2017-project-1/master/src/schedule.json';
+        self.dataList = '';
 
         /**
          * Retrieve data for schedule
          */
         self.getData = function() {
-            console.log('get data');
+            var xhr = new XMLHttpRequest();
+
+            xhr.open('GET', self.endPoint, true);
+
+            xhr.send();
+
+            xhr.onreadystatechange = function() {
+                if (this.readyState !== 4) {
+                    return
+                }
+
+                if (this.status !== 200) {
+                    return;
+                }
+
+                console.log(this.responseText);
+
+                // self.dataList = JSON.parse(this.responseText);
+
+                // console.log(self.dataList);
+
+                // self.render();
+            };
+        };
+
+        /**
+         * Render schedule
+         */
+        self.render = function() {
+            console.log(self.dataList);
         };
 
         /**
