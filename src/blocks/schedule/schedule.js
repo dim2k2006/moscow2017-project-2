8088d25efe61ba7ef93443452e1067dd79075113
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-03-21 21:50:13 +0300

    Update static

--- src/blocks/schedule/schedule.js
+++ src/blocks/schedule/schedule.js
@@ -46,7 +46,8 @@
          */
         self.render = function(list) {
             list.forEach(function(item) {
-                var element = document.createElement('li');
+                var element = document.createElement('li'),
+                    schools = '';
 
                 element.classList.add('scheduleList__item');
                 element.innerHTML = self.template;
@@ -54,9 +55,14 @@
                 element.querySelector('.scheduleItem__date').innerHTML = item.data;
                 element.querySelector('.scheduleItem__time').innerHTML = item.time;
                 element.querySelector('.scheduleItem__title').innerHTML = item.title;
-                // element.querySelector('.scheduleItem__school').innerHTML = item.title; // school need loop
                 element.querySelector('.scheduleItem__author').innerHTML = item.author.name;
                 element.querySelector('.scheduleItem__place').innerHTML = item.place;
+
+                item.school.forEach(function(item) {
+                    schools += '<span class="scheduleItem__' + item.id +'">'+ item.name +'</span>';
+                });
+
+                element.querySelector('.scheduleItem__school').innerHTML = schools;
 
                 self.list.appendChild(element); // create set of items and insert them in html after loop only once
             });
