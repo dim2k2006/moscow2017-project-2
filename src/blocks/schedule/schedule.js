4f4a0f4a85735f3d843e8b33a76d1fc88c8bf538
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-03-21 23:04:02 +0300

    Add styles for block scheduleItem

--- src/blocks/schedule/schedule.js
+++ src/blocks/schedule/schedule.js
@@ -13,7 +13,21 @@
         self.container = document.querySelector('.schedule');
         self.endPoint = 'https://raw.githubusercontent.com/dim2k2006/moscow2017-project-1/master/src/schedule.json?cache=' + Math.random();
         self.template = document.querySelector('#scheduleItem-template').innerHTML;
-        self.list = document.querySelector('.scheduleList__list');
+        self.content = document.querySelector('.scheduleList__content');
+        self.monthNames = [
+            'январь',
+            'февраль',
+            'март',
+            'апрель',
+            'май',
+            'июнь',
+            'июль',
+            'август',
+            'сентябрь',
+            'октябрь',
+            'ноябрь',
+            'декабрь'
+        ];
         self.dataList = '';
 
         /**
@@ -45,14 +59,22 @@
          * Render schedule
          */
         self.render = function(list) {
+            var content = document.createElement('ul');
+
+            content.classList.add('scheduleList__list');
+
             list.forEach(function(item) {
                 var element = document.createElement('li'),
-                    schools = '';
+                    schools = '',
+                    date = new Date(item.date),
+                    day = date.getDate();
+                    month = self.monthNames[date.getMonth()];
 
                 element.classList.add('scheduleList__item');
                 element.innerHTML = self.template;
 
-                element.querySelector('.scheduleItem__day').innerHTML = item.date;
+                element.querySelector('.scheduleItem__day').innerHTML = day;
+                element.querySelector('.scheduleItem__month').innerHTML = month;
                 element.querySelector('.scheduleItem__time').innerHTML = item.time;
                 element.querySelector('.scheduleItem__title').innerHTML = item.title;
                 element.querySelector('.scheduleItem__author').innerHTML += item.author.name;
@@ -64,8 +86,10 @@
 
                 element.querySelector('.scheduleItem__school').innerHTML = schools;
 
-                self.list.appendChild(element); // create set of items and insert them in html after loop only once
+                content.appendChild(element);
             });
+
+            self.content.appendChild(content);
         };
 
         /**
